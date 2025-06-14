9b1d862a182f5b46b49bf9dceaf197e62260dcf4
Jeijesh <jeshica.anak.baik.hati@gmail.com>
2025-06-14 14:39:06 +0700

    Update profile.blade.php

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,19 +1,26 @@
-@extends('master')
+@extends('master') {{-- Menggunakan layout utama --}}
 
-@section('title', 'Profil Saya')
+@section('title', 'Profil Saya') {{-- Set judul halaman --}}
+
+{{-- Jika path URL adalah /profile, maka tambahkan CSS khusus --}}
 @if (Request::is('profile'))
     <link rel="stylesheet" href="{{ asset('css/profile.css') }}">
 @endif
+
 @section('content')
 <div class="profile-container">
     <div class="profile-card">
+        {{-- Menampilkan nama dan email user yang sedang login --}}
         <h2>{{ Auth::user()->name }}</h2>
         <p class="profile-email">{{ Auth::user()->email }}</p>
 
         <h3 class="section-title">Beasiswa yang Diikuti</h3>
+
+        {{-- Jika user belum mendaftar beasiswa --}}
         @if($scholarships->isEmpty())
             <p class="no-scholarship">Belum mengikuti beasiswa apapun.</p>
         @else
+            {{-- Menampilkan daftar beasiswa yang diikuti user --}}
             <ul class="scholarship-list">
                 @foreach($scholarships as $scholarship)
                     <li class="scholarship-item">
@@ -22,6 +29,7 @@
                             <span class="scholarship-organization">{{ $scholarship->organization }}</span>
                         </div>
                         <div class="scholarship-details">
+                            {{-- Format tanggal pendaftaran dan deadline --}}
                             <p><strong>Pendaftaran:</strong> {{ \Carbon\Carbon::parse($scholarship->open_registration)->format('d M Y') }}</p>
                             <p><strong>Deadline:</strong> {{ \Carbon\Carbon::parse($scholarship->deadline)->format('d M Y') }}</p>
                         </div>
